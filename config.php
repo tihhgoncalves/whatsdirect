8c24334471a06bdca0daee5ba5f730b000ea5c91
Tihh Gonçalves <tihhgoncalves@gmail.com>
2019-03-11 19:08:25 -0300

    Atualizando pra WhatsDirect.

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $config->set('site_production_url',  'http://publico.taticadesucesso.com.br/site
 $config->set('site_beta_url',        'http://www.site.com.br');
 
 $config->set('site_path',            __DIR__ . '/');
-$config->set('page_title',           'DirectWhats // Envie mensagem no WhatsApp sem registrar o contato na sua agenda.');
+$config->set('page_title',           'WhatsDirect // Envie mensagem no WhatsApp sem registrar o contato na sua agenda.');
 $config->set('page_language',        'pt_BR');
 $config->set('page_description',     'Um jeito fácil de mandar mensagem pra alguém que não está na sua agenda. Basta registrar o número e clicar no botão e a janela do contato já aparecerá no seu WhatsApp.');
 $config->set('page_type',            'website');
